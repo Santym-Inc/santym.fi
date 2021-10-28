7e81e17ca721d2a6c8dd601b96c7001840db3453
yeabu369 <yeabsdebe@gmail.com>
2021-10-28 13:36:57 +0300

    Removed lock component

--- components/index.ts
+++ components/index.ts
@@ -4,7 +4,6 @@ export * from './copy-text';
 export * from './dropdown';
 export * from './input';
 export * from './layout';
-export * from './lock';
 export * as toast from './toast';
 export * from './toggle';
 export * from './table';
